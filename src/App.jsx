85e85e51348105e5cdcf0e149ea8d88bfeeb619e
Kamui-Hebertt <heberttaferreira.com@gmail.com>
2023-07-27 23:52:09 -0300

    feat: preparing enviroment, matine, cube, orbitContrrols

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+
+
+import { Canvas } from "@react-three/fiber";
+import Experience from "./Experience";
+
+function App() {
+
+
+  return (
+    <Canvas>
+ 
+
+    <Experience />
+  </Canvas>
+  )
+}
+
+export default App
